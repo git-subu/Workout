21fd224eab68ad36d358537840d6c4458eaf2102
vijayasubash2000@gmail.com <vijayasubash2000@gmail.com>
2022-05-15 23:24:27 +0530

    Question1 emit

---
+++ src/components/EmitChild.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="BlogPage">
+    <h1 class="heading-text">Welcome</h1>
+    <div class="parent-container">
+      <div class="Card-body">
+        <Content
+          title="Introduction to CSS"
+          content="CSS is a stylesheet language used to style the structure of our document"
+          @displayData="fetchData($event)"
+        />
+      </div>
+      <div class="Card-body">
+        <Content
+          title="What is $Emit in Vue Js"
+          content="Vue $emit is a function that lets us emit, or send, custom events "
+          @displayData="fetchData($event)"
+        />
+      </div>
+      <div class="Card-body">
+        <Content
+          title="Understanding Currying"
+          content="Currying simply means evaluating functions with multiple arguments"
+          @displayData="fetchData($event)"
+        />
+      </div>
+      </div>
+      <h4 class="result-text">Result: {{ result }}</h4>
+   
+  </section>
+ 
+</template>
+<script>
+import Content from "../components/Content.vue";
+export default {
+  name: "BlogPage",
+  components: {
+    Content,
+  },
+  data: () => ({
+    result: "",
+  }),
+  methods: {
+    fetchData(data) {
+      this.result = data;
+    },
+  },
+};
+</script>
+      
+ 
---
+++ src/components/EmitParent.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="Example for emit-parent">
+    <h1 class="title-text">{{ title }}</h1>
+    <p class="content-text">{{ content }}</p>
+  <button v-on:click="displayContent" class="text-link">See More</button>
+  
+  </section>
+</template>
+// JS File
+<script>
+export default {
+  name: "BlogContent",
+  props: ["title", "content"],
+   data() {},
+  methods: {
+    displayContent() {
+      this.$emit("displayData", this.title);
+    },
+  },
+
+};
+
+</script>
